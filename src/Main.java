77fea2efade62022776233d60df943bbba562f86
asloba <aslobaa@mail.ru>
2021-03-10 22:45:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 18_500;
+        int expense = 20;
+        int totalMiles = price / expense;
+        System.out.println(totalMiles);
+    }
+}
